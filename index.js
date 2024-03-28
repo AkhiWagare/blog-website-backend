7976558880fd461753916222c99e7b0959ed8fc1
Akhi <akhileshwagare1102@gmail.com>
2024-03-29 00:36:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,154 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const User = require('./models/User')
+const { create } = require("domain")
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const multer = require('multer')
+const fs = require('fs')
+const Post = require('./models/Post')
+
+require('dotenv').config()
+const port = process.env.PORT || 3001
+
+const uploadMiddleware = multer({ dest: 'uploads/' })
+
+const salt = bcrypt.genSaltSync(10);
+
+const app = express()
+app.use(cors({ credentials: true, origin: 'http://localhost:5173' }))
+
+app.use(cookieParser())
+
+app.set("view engine", "ejs");
+app.use(express.json())
+app.use('/uploads', express.static(__dirname + '/uploads'))
+
+mongoose.connect(process.env.MONGO_URI)
+
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password, salt)
+        })
+        res.json(userDoc)
+    } catch (e) {
+        res.status(400).json(e)
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    const userDoc = await User.findOne({ username })
+    const passOk = bcrypt.compareSync(password, userDoc.password)
+
+    if (passOk) {
+        jwt.sign({ username, id: userDoc._id }, process.env.SECRET_KEY, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json({
+                id: userDoc._id,
+                username
+            })
+        })
+    } else {
+        res.status(400).json({
+            message: 'Invalid username or password'
+        })
+    }
+})
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, process.env.SECRET_KEY, {}, (err, info) => {
+        if (err) throw err
+        res.json(info)
+    })
+})
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok')
+})
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const {originalname, path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path+'.'+ext;
+    fs.renameSync(path, newPath)
+
+    const {token} = req.cookies;
+    jwt.verify(token, process.env.SECRET_KEY, {}, async (err, info) => {
+        if (err) throw err
+
+        const {title, summary, content} = req.body
+
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id
+        })
+        res.json(postDoc)
+    })
+})
+
+app.get('/post', async (req, res) => {
+    res.json(
+        await Post.find()
+        .populate('author', ['username'])
+        .sort({createdAt: -1})
+        .limit(20)
+    )
+})
+
+app.get('/post/:id', async (req, res) => {
+    const {id} = req.params;
+    const postDoc = await Post.findById(id)
+        .populate('author', ['username'])
+    res.json(postDoc)
+})
+
+app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+    let newPath = null;
+    if (req.file) {
+        const {originalname, path} = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        newPath = path+'.'+ext;
+        fs.renameSync(path, newPath)
+    }
+
+    const {token} = req.cookies;
+    jwt.verify(token, process.env.SECRET_KEY, {}, async (err, info) => {
+        if (err) throw err
+
+        const {id, title, summary, content} = req.body
+        const postDoc = await Post.findById(id)
+        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id)
+        
+        if (!isAuthor) {
+            return res.status(400).json("You aren't the Author!")
+        }
+
+        await postDoc.updateOne({
+            title,
+            summary,
+            content,
+            cover : newPath ? newPath : postDoc.cover
+        })
+
+        res.json(postDoc)
+    })
+})
+
+app.get('/', (req, res) => res.send("Hello"))
+
+app.listen(port, (req, res) => {
+    console.log(`Server is listening on port ${port}...`)
+});
